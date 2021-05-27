f701bf2c55188f80634875cf985cdf7051590a65
kottok2205 <tarasyuk222001@gmail.com>
2021-05-27 23:02:58 +0300

    1

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading;
+
+namespace Lab_3_3_
+{
+    class Program
+    {
+        static void Go(object num)
+        {
+            int n = (int)num;
+            if (n <= 0)
+                return;
+            Thread t = new Thread(Go);
+            t.Name = "Потоки" + n;
+            t.Start(n - 1);
+            Console.WriteLine(t.Name);
+        }
+        private static void Main(string[] args)
+        {
+            Go(10);
+            Console.ReadKey();
+        }
+    }
+}
